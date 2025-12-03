442c9a90feb28351eaa54263d50e8c0388c0326d
rayhong118 <rayhong118@gmail.com>
2025-12-03 12:03:41 -0800

    use tailwind css handle focus

--- src/common/components/Navigation.tsx
+++ src/common/components/Navigation.tsx
@@ -18,32 +18,26 @@ export const Navigation = () => {
         </button>
         <button
           onClick={() => navigate("/experiments")}
-          onMouseEnter={() => setIsExperimentsOpen(true)}
-          onFocus={() => setIsExperimentsOpen(true)}
-          onMouseLeave={() => setIsExperimentsOpen(false)}
-          onBlur={() => setIsExperimentsOpen(false)}
-          className="cursor-pointer hover:underline p-2 relative"
+          className="cursor-pointer hover:underline p-2 relative group"
         >
           Experiments
-          {isExperimentsOpen && (
-            <menu className="absolute top-10 left-0 bg-white border rounded-md shadow-md flex flex-col ">
-              <li>
-                <button className=" cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
-                  Copy
-                </button>
-              </li>
-              <li>
-                <button className=" cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
-                  Cut
-                </button>
-              </li>
-              <li>
-                <button className=" cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
-                  Paste
-                </button>
-              </li>
-            </menu>
-          )}
+          <menu className="absolute top-10 left-0 bg-white ring-black ring-opacity-5 rounded-md shadow-md flex flex-col w-48 py-2 scale-0 group-hover:scale-100 group-focus:scale-100 duration-300 origin-top">
+            <li className="block">
+              <button className="block cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
+                Copy
+              </button>
+            </li>
+            <li className="block">
+              <button className="block cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
+                Cut
+              </button>
+            </li>
+            <li className="block">
+              <button className="block cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
+                Paste
+              </button>
+            </li>
+          </menu>
         </button>
       </nav>
     </div>
