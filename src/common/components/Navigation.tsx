2542dc2e3b68d8e2ef2a9cd416401866b8d8504d
rayhong118 <rayhong118@gmail.com>
2025-12-03 15:05:59 -0800

    form

--- src/common/components/Navigation.tsx
+++ src/common/components/Navigation.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 
 export const Navigation = () => {
   const navigate = useNavigate();
-  const [isExperimentsOpen, setIsExperimentsOpen] = useState(false);
   return (
     <div className="fixed top-0 w-screen p-2 bg-white shadow-md flex flex-row px-10">
       <button onClick={() => navigate("/")} className="cursor-pointer p-2">
@@ -17,14 +16,17 @@ export const Navigation = () => {
           About
         </button>
         <button
-          onClick={() => navigate("/experiments")}
+          // onClick={() => navigate("/experiments")}
           className="cursor-pointer hover:underline p-2 relative group"
         >
           Experiments
           <menu className="absolute top-10 left-0 bg-white ring-black ring-opacity-5 rounded-md shadow-md flex flex-col w-48 py-2 scale-0 group-hover:scale-100 group-focus:scale-100 duration-300 origin-top">
             <li className="block">
-              <button className="block cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full">
-                Copy
+              <button
+                className="block cursor-pointer hover:bg-gray-100 p-1 rounded-md w-full"
+                onClick={() => navigate("/experiments/formValidation")}
+              >
+                Form Validation
               </button>
             </li>
             <li className="block">
--- src/pages/experiments/FormValidation.tsx
+++ src/pages/experiments/FormValidation.tsx
@@ -1,18 +1,99 @@
+import { useState } from "react";
+
+interface IFormData {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  date?: string;
+}
+
 export const FormValidation = () => {
+  const [formData, setFormData] = useState<IFormData>();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const clearForm = () => {
+    setFormData({});
+  };
+
+  const submitForm = () => {
+    console.log("Form submitted:", formData);
+  };
   return (
-    <form className={"p-20"}>
-      <h1 className={"text-2xl font-bold mb-4"}>Form Validation Experiment</h1>
-      <div className={"mb-4"}>
-        <label className={"block mb-2"} htmlFor="email">
-          Email:
-        </label>
+    <div className={"p-20"}>
+      <form action={() => submitForm()}>
+        <h1 className={"text-2xl font-bold mb-4"}>
+          Form Validation Experiment
+        </h1>
+        <div className={"mb-4"}>
+          <label className={"block mb-2"} htmlFor="email">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData?.email || ""}
+            className={"border p-2 w-full"}
+            onChange={handleChange}
+            required
+          />
+          <label className={"block mb-2 mt-4"} htmlFor="firstName">
+            First Name:
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData?.firstName || ""}
+            className={"border p-2 w-full"}
+            onChange={handleChange}
+            required
+          />
+          <label className={"block mb-2 mt-4"} htmlFor="lastName">
+            Last Name:
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData?.lastName || ""}
+            className={"border p-2 w-full"}
+            onChange={handleChange}
+            required
+          />
+          <label className={"block mb-2 mt-4"} htmlFor="phoneNumber">
+            Date:
+          </label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={formData?.date || ""}
+            className={"border p-2 w-full"}
+            onChange={handleChange}
+            required
+          />
+        </div>
         <input
-          type="email"
-          id="email"
-          className={"border p-2 w-full"}
-          required
-        />
-      </div>
-    </form>
+          type="submit"
+          className="cursor-pointer hover:opacity-50 border border-gray-300 shadow-md rounded-md w-24 py-2 mix-blend-darken"
+          value="Submit"
+        ></input>
+        <input
+          type="button"
+          className="cursor-pointer hover:opacity-50 border border-gray-300 shadow-md rounded-md w-24 py-2 mix-blend-darken"
+          value="Clear"
+          onClick={clearForm}
+        ></input>
+      </form>
+
+      <h1>Form Data</h1>
+      <pre>{JSON.stringify(formData, null, 2)}</pre>
+    </div>
   );
 };
