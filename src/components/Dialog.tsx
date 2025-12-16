692ca3dc5cb1141a248e548ad31be3af99590c68
rayhong118 <rayhong118@gmail.com>
2025-12-16 14:39:06 -0800

    dialog-allow place autocomplete select, disable bg scroll

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,6 +1,7 @@
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
 import { FocusTrap } from "focus-trap-react";
 import { SecondaryButton } from "./Buttons";
 
@@ -18,47 +19,77 @@ interface IDialogProps {
   children?: React.ReactNode;
 }
 
+
+
 export const Dialog: React.FC<IDialogProps> = (props: IDialogProps) => {
   const { title, actions, open, onClose, children } = props;
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [open]);
+
   if (!open) return null;
+
   return ReactDOM.createPortal(
     <>
       <div className="fixed inset-0 bg-black/50 backdrop-grayscale z-40" />
-      <dialog
-        id="dialog"
-        open={open}
-        aria-modal="true"
-        role="dialog"
-        className="border rounded-md flex flex-col space-around justify-between gap-0 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0 bg-white shadow-xl z-50 w-full max-w-lg max-h-[calc(100vh-4rem)]"
-      >
-        <div className="flex flex-row justify-between items-center px-5 pt-5">
-          <h1 className="text-xl font-bold">{title}</h1>
-          <SecondaryButton
-            onClick={onClose}
-            className="cursor-pointer hover:bg-gray-100 border-gray-200 border rounded-md p-2"
-          >
-            <FontAwesomeIcon icon={faClose} className="h-6 w-6 text-gray-600" />
-          </SecondaryButton>
-        </div>
-
-        <div className="p-5 overflow-y-scroll">
-          {children}
-        </div>
-        {actions && (
-          <div className="flex flex-row justify-end gap-2 px-5 pb-5">
-            {actions.map((action, index) => (
-              <button
-                key={index}
-                onClick={action.onClick}
-                className="cursor-pointer hover:bg-gray-100 px-4 py-2 border-gray-200 border-1 rounded-md"
-              >
-                {action.label}
-              </button>
-            ))}
+      <FocusTrap
+        focusTrapOptions={{
+          // This function allows clicks on the Google results to go through
+          allowOutsideClick: (event: any) => {
+            console.log("event", event);
+            if (
+              event.target?.classList.contains('pac-item') ||
+              event.target?.classList.contains('pac-container') ||
+              event.target?.closest('.pac-container')
+            ) {
+              return true;
+            }
+            return false;
+          },
+        }}>
+        <dialog
+          id="dialog"
+          open={open}
+          aria-modal="true"
+          role="dialog"
+          className="border rounded-md flex flex-col space-around justify-between gap-0 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0 bg-white shadow-xl z-50 w-full max-w-lg max-h-[calc(100vh-4rem)]"
+        >
+          <div className="flex flex-row justify-between items-center px-5 pt-5">
+            <h1 className="text-xl font-bold">{title}</h1>
+            <SecondaryButton
+              onClick={onClose}
+              className="cursor-pointer hover:bg-gray-100 border-gray-200 border rounded-md p-2"
+            >
+              <FontAwesomeIcon icon={faClose} className="h-6 w-6 text-gray-600" />
+            </SecondaryButton>
           </div>
-        )}
-      </dialog>
 
+          <div className="p-5 overflow-y-scroll">
+            {children}
+          </div>
+          {actions && (
+            <div className="flex flex-row justify-end gap-2 px-5 pb-5">
+              {actions.map((action, index) => (
+                <button
+                  key={index}
+                  onClick={action.onClick}
+                  className="cursor-pointer hover:bg-gray-100 px-4 py-2 border-gray-200 border-1 rounded-md"
+                >
+                  {action.label}
+                </button>
+              ))}
+            </div>
+          )}
+        </dialog>
+      </FocusTrap>
     </>
     , document.body);
 };
