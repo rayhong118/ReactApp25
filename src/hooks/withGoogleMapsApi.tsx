58d8514dc7df2b7d0611cf6ad2a03b40838592fa
rayhong118 <rayhong118@gmail.com>
2025-12-16 19:42:01 -0800

    performance warning mitigation

--- src/hooks/withGoogleMapsApi.tsx
+++ src/hooks/withGoogleMapsApi.tsx
@@ -1,12 +1,15 @@
 import { googleMapsApiKey } from "@/apikeys";
 import { useJsApiLoader } from "@react-google-maps/api";
 
+type Library = "places" | "drawing" | "geometry" | "visualization";
+const LIBRARIES: Library[] = ["places"];
+
 export function withGoogleMapsApi(WrappedComponent: React.ComponentType) {
   return function (props: any) {
     const { isLoaded } = useJsApiLoader({
       id: "google-map-script",
       googleMapsApiKey: googleMapsApiKey,
-      libraries: ["places"]
+      libraries: LIBRARIES
     });
     return (
       isLoaded ?
