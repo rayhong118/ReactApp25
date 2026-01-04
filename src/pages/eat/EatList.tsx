01170828bdd6f81f01769f2edf6c2967ca82c98a
rayhong118 <rayhong118@gmail.com>
2026-01-04 13:34:40 -0800

    when display eat by id hide sort dropdown

--- src/pages/eat/EatList.tsx
+++ src/pages/eat/EatList.tsx
@@ -75,38 +75,40 @@ export const EatList = () => {
         <FontAwesomeIcon icon={faPlus} className="mr-2" />
         {t("eat.list.addRestaurant")}
       </PrimaryButton>
-      <div className="flex gap-2 items-center">
-        <div>{t("eat.list.orderBy")}</div>
-        <select
-          onChange={(e) => {
-            const value = e.target.value;
-            if (!value) {
-              setOrderBy(undefined);
-              return;
-            }
-            const [field, direction] = value.split(",");
-            setOrderBy({
-              field,
-              direction: direction as "asc" | "desc",
-            });
-          }}
-          value={orderBy?.field + "," + orderBy?.direction}
-          className="border border-gray-300 rounded p-2"
-        >
-          <option className="p-2" value={""}>
-            {t("eat.list.orderByNone")}
-          </option>
-          <option className="p-2" value={"averageStars,desc"}>
-            {t("eat.list.orderByAverageRating")}
-          </option>
-          <option className="p-2" value={"price,asc"}>
-            {t("eat.list.orderByPriceLowToHigh")}
-          </option>
-          <option className="p-2" value={"price,desc"}>
-            {t("eat.list.orderByPriceHighToLow")}
-          </option>
-        </select>
-      </div>
+      {!eatQuery.id && (
+        <div className="flex gap-2 items-center">
+          <div>{t("eat.list.orderBy")}</div>
+          <select
+            onChange={(e) => {
+              const value = e.target.value;
+              if (!value) {
+                setOrderBy(undefined);
+                return;
+              }
+              const [field, direction] = value.split(",");
+              setOrderBy({
+                field,
+                direction: direction as "asc" | "desc",
+              });
+            }}
+            value={orderBy?.field + "," + orderBy?.direction}
+            className="border border-gray-300 rounded p-2"
+          >
+            <option className="p-2" value={""}>
+              {t("eat.list.orderByNone")}
+            </option>
+            <option className="p-2" value={"averageStars,desc"}>
+              {t("eat.list.orderByAverageRating")}
+            </option>
+            <option className="p-2" value={"price,asc"}>
+              {t("eat.list.orderByPriceLowToHigh")}
+            </option>
+            <option className="p-2" value={"price,desc"}>
+              {t("eat.list.orderByPriceHighToLow")}
+            </option>
+          </select>
+        </div>
+      )}
 
       {allRestaurants?.map((restaurant: IRestaurant) => (
         <EatCard key={restaurant.id} restaurant={restaurant} />
